56b607ced4d1f469ae70dd2d8fae5a1b1dc1b658
Artem <artem@test.email>
2025-08-01 22:12:49 +0300

    Земля в иллюминаторе

--- soil/tiles.gd
+++ soil/tiles.gd
@@ -1,48 +1,28 @@
 extends Node2D
 
-#@export var tile_texture := preload("res://soil/soil texture.jpg")
-@export var tile_size := Vector2(86, 86)
-@export var grid_width : int = 8
-@export var grid_height : int = 4
+@export var tile_texture : Texture2D
+@export var tile_size := Vector2(86, 86) # размер тайла
+@export var grid_width : int = 16 # длина
+@export var grid_height : int = 4 # ширина
 ########################################
-#var wetness : float = 0.
-#var capacity : int = 1
-#var coef_of_minerals : float = 0.
-
-#var grid : Array [Vector2]
-#var grid : Array [Sprite2D]
-var grid : Array [Array]
+@export var tile : PackedScene
+var grid : Array [Array] # матрица тайлов
 ########################################
 
 func _ready():
 	create_tile_grid()
-	#badass()
 
 func create_tile_grid():
 	for y in grid_height:
 		var row : Array [Sprite2D]
 		for x in grid_width:
-			var tile := Sprite2D.new()
-			tile.texture = preload("res://soil/soil texture small.jpg")
+			var tile := tile.instantiate()
+			tile.texture = tile_texture
+			tile.modulate = Color(0.1, 0.1, 0.1) * (grid_height - y)
 			tile.position = Vector2(x, y) * tile_size
-			tile.set_meta("wetness", randf_range(0.1, 1))
-			tile.set_meta("capacity", 1)
-			tile.set_meta("coef_of_minerals", randf_range(0.1, 1))
-			#grid.append(Vector2(tile.x, tile.y))
-			#grid.append(tile)
+			tile.wetness = randf_range(0.1, 1)
+			tile.capacity = 1
+			tile.coef_of_minerals = randf_range(0.1, 1)
 			row.append(tile)
 			add_child(tile)
 		grid.append(row)
-
-#func badass():
-	#for x in 32:
-			#print(grid[x].get_meta("wetness"))
-			#print(grid[x].get_meta("capacity"))
-			#print(grid[x].get_meta("coef_of_minerals"))
-
-#func badass():
-	#for y in grid_height:
-		#for x in grid_width:
-			#print(grid[y][x].get_meta("wetness"))
-			#print(grid[y][x].get_meta("capacity"))
-			#print(grid[y][x].get_meta("coef_of_minerals"))
---
+++ tile.gd
@@ -0,0 +1,5 @@
+extends Sprite2D
+
+var wetness : float = 0. # влажность
+var capacity : int = 1 # вместимость
+var coef_of_minerals : float = 0. # коэффициент минералов
