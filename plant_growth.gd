65fca9855ff684e720d38bcb0875bcb7178d0ca4
Klaim <arhik2.0@gmail.com>
2025-08-01 14:54:07 +0300

    Исправил баги и добавил растениям жизни

--- plant_growth.gd
+++ plant_growth.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 ###########################################################
-@export_category("Plant Properties")
+@export_group("Plant Properties")
 @export var growth_textures : Array[Texture2D] # Текстуры растений (Количество текстур = количеству стадий)
 @export var speed : float = 0.1 # Скорость роста
 @export var optimal_moisture: Vector2 = Vector2(0.4, 0.7) # Оптимальная влажность почвы, при котором растение растет
@@ -10,23 +10,25 @@ extends Node2D
 @export var absorption_rate: float = 0.1 # Сколько воды оно собирает
 @export var max_water: float = 1. # Максимальное количество собранной воды
 @export var is_day_plant: bool = true # Дневное ли это растение
-
+@export var max_health: int = 30 # Максимальное количество жизни у растения
 
 ###########################################################
-@export_category("Plant state")
+@export_group("Plant state")
 @export var stage : int = 0 # Стадия роста
 @export var growth: float = 0. # Рост
 @export var energy: float = 0. # Энергия
 @export var water: float = 0. # Вода
+@export var health: float = max_health # Количествно жизни
  
 ###########################################################
-@export_category("External var")
+@export_group("External var")
 @export var weather_factor: float = 1. # Фактор погоды
-@export var soil_moisture: float = 0.0  # Влажность почвы
+@export var soil_moisture: float = 0.5  # Влажность почвы
 @export var is_day: bool = true # День или ночь
 
 ###########################################################
 var can_grow: bool = true # Может ли расти растение
+var dying: bool = false # Умирает ли растение
 
 func _ready() -> void:
 	$Texture.texture = growth_textures[0]
@@ -35,39 +37,53 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if (can_grow):
 		process_growth(delta)
+	else:
+		dying = true
+	die_process(delta)
 
 func absorb_water(delta: float) -> void:
 	water += absorption_rate * delta
 	water = clamp(water, 0.0, max_water + max_water * 0.25)
 
-func die():
-	can_grow = false
+func die_process(delta: float):
+	if (dying):
+		health -= 1 * delta
+	else:
+		health += 1 * delta
+		if (health > max_health):
+			health = max_health
 
 func suitable_moisture() -> bool:
 	return soil_moisture >= optimal_moisture.x && soil_moisture <= optimal_moisture.y
 
-func drink_water() -> void:
-	water -= required_water
+func drink_water(delta: float) -> void:
+	water -= required_water * delta
 	if (water < 0):
-		die()
+		water = 0
+		dying = true
+	else: 
+		dying = false
 
 func process_growth(delta: float) -> void:
-	if ((is_day == is_day_plant) && can_grow):
-		if(suitable_moisture()):
-			if (stage == 0):
-				growth += speed * weather_factor * delta
+	if (is_day == is_day_plant):
+		if (stage == 0):
+			growth += speed * weather_factor * delta
+			if(suitable_moisture()):
 				absorb_water(delta)
-				drink_water()
-			else:
-				growth += speed * weather_factor * delta
-				energy += speed * energy_conservation
+			drink_water(delta)
+		else:
+			growth += speed * weather_factor * delta
+			energy += speed * energy_conservation * delta
+			if(suitable_moisture()):
 				absorb_water(delta)
-				drink_water()
+			drink_water(delta)
 	else:
 		if (energy > 0):
 			growth += speed * weather_factor * delta
-			energy -= speed
-			drink_water()
+			energy -= speed * delta
+			if(suitable_moisture()):
+				absorb_water(delta)
+			drink_water(delta)
 	
 	if (growth >= 1):
 		growth = 0
@@ -75,4 +91,4 @@ func process_growth(delta: float) -> void:
 		if (stage < growth_textures.size()):
 			$Texture.texture = growth_textures[stage]
 		else:
-			die()
+			dying = true
