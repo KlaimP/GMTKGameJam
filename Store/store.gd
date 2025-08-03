3044b111f48a22f38179955dba177f655d64872b
Klaim <arhik2.0@gmail.com>
2025-08-03 18:35:43 +0300

    Бля, Артем

--- Store/store.gd
+++ Store/store.gd
@@ -7,17 +7,17 @@ var money: int = 5 # кол-во денег
 func _ready():
 	update_currency_label()
 	
-	$Item1.pressed.connect(func(): buy_item(1, "item_name1"))
-	$Item2.pressed.connect(func(): buy_item(1, "item_name1"))
-	$Item3.pressed.connect(func(): buy_item(1, "item_name1"))
+	$Item1.pressed.connect(func(): buy_item(1, "carnifex", ResourceLoader.load("res://plants/types/carnifex.tres")))
+	$Item2.pressed.connect(func(): buy_item(1, "item_name2", ResourceLoader.load("res://plants/types/cucold.tres")))
+	$Item3.pressed.connect(func(): buy_item(1, "item_name3", ResourceLoader.load("res://plants/types/gib.tres")))
 	
 	$BackButton.pressed.connect(_on_back_pressed)
 
-func buy_item(cost: int, item_name: String):
+func buy_item(cost: int, item_name: String, type: PlantType):
 	if money >= cost and inventory.inventory.size() != inventory.max_slots:
 		money -= cost
 		var plant: Card
-		inventory.add_card(plant) # добавление карты (входные параметры в класс?)
+		inventory.add_card(plant, type) # добавление карты (входные параметры в класс?)
 		update_currency_label()
 
 func update_currency_label():
--- UI/inventory.gd
+++ UI/inventory.gd
@@ -5,14 +5,16 @@ class_name Inventory
 @export var inventory: Array[Card]
 @export var card_slot: PackedScene
 @export var max_slots: int
+@export var plants: PackedScene
 
-func add_card(card: Card):
+func add_card(card: Card, type_plant: PlantType):
 	if (inventory.size() >= max_slots):
 		return
 	var slot = card_slot.instantiate()
-	slot.itemName = "Bebebe"
-	var plant: Plant
+	var plant = plants.instantiate()
+	plant.set_type(type_plant)
 	
+	slot.itemName = "Bebebe"
 	slot.plant = plant
 	inventory.append(slot)
 	$BoxContainer/VBoxContainer.add_child(slot)
